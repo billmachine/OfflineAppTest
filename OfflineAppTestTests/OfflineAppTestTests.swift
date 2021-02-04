769f357ac4b03df67f50ea99dbf0fab316f1ee1f
billi <ahmad.billi@nostratech.com>
2021-02-05 05:38:58 +0700

    Adding unit test

---
+++ OfflineAppTestTests/OfflineAppTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  OfflineAppTestTests.swift
+//  OfflineAppTestTests
+//
+//  Created by Ahmad Billi Afandi on 05/02/21.
+//  Copyright © 2021 nostratech. All rights reserved.
+//
+
+import XCTest
+
+class OfflineAppTestTests: XCTestCase {
+
+    func testInvalidInputSides() {
+        XCTAssertThrowsError(try detectTriangle(-1, 2, 3)) { error in
+            XCTAssertEqual(error as? TriangleError, TriangleError.invalidInput)
+        }
+    }
+    
+    func detectTriangle(
+        _ sideA: Int,
+        _ sideB: Int,
+        _ sideC: Int
+    ) throws {
+        if sideA < 1 {
+            throw TriangleError.invalidInput
+        }
+    }
+    
+}
+
+enum TriangleError: Error {
+    case invalidInput
+}
+
